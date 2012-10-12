63b108d9a53e31f7ec0012958ba63fee9eee3247
Josh Fernandes <joshfernandes@mac.com>
2012-10-11 17:37:50 -0700

    see if this works

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ def hello():
     data = json.dumps(dict)
     return data
 
+@app.route('/physics/standardgravity')
+def hello():
+    dict = {'value': 9.80665, 'units': 'meters/(seconds^2)', 'citation': 'http://en.wikipedia.org/wiki/Standard_gravity','name': 'standard gravity'};
+    data = json.dumps(dict)
+    return data
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
