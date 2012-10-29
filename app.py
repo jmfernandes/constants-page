e61646e162914371c65019205ffd5252a3bb4c24
Josh Fernandes <joshfernandes@mac.com>
2012-10-28 23:12:51 -0700

    hey

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def index():
 
 @app.route('/physics/planck_constant', endpoint='planck_constant')
 def hello():
+    def hello2():
+        return "hello"
     dict = {'value': 6.62606957E-34, 'units': 'joule*seconds', 'citation': 'http://en.wikipedia.org/wiki/Planck_constant','name': 'planck constant'};
     data = json.dumps(dict)
     return data
