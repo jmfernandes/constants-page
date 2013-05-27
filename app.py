e21325e87511e520fadbb77a7ee75badd1bef308
Josh Fernandes <joshfernandes@mac.com>
2013-05-26 22:03:53 -0700

    trying to fix dog data

--- app.py
+++ app.py
@@ -367,7 +367,7 @@ def index():
 
 @app.route('/biology/dog_ld50_theobromine_json', endpoint='/biology/dog_ld50_theobromine_json')
 def index():
-    return render_template('json/biology/dog_ld50_theobromine.json')
+    return render_template('json/biology/hi.json')
 
 
 """cat LD50"""
